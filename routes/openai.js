2620be0ed7ad1bed9ef328849a06aa971d78ffb3
Dmitry Sky <dmitry.sky@icloud.com>
2023-05-04 13:36:24 -0600

    Make a request to gpt-3.5-turbo and post the response to chatengine

--- routes/openai.js
+++ routes/openai.js
@@ -9,10 +9,34 @@ const router = express.Router();
 router.post("/text", async (req, res) => {
     try {
         const { text, activeChatId } = req.body;
-        console.log("text: ", text);
-        res.status(200).json({ text });
+
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "system", content: "You are a helpful assistant." },
+                { role: "user", content: text },
+            ],
+        });
+
+        console.log("RESPONSE", response.data.choices[0].message.content);
+
+        await axios.post(
+            `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+            { text: response.data.choices[0].message.content },
+            {
+                headers: {
+                    "Project-ID": process.env.PROJECT_ID,
+                    "User-Name": process.env.BOT_USER_NAME,
+                    "User-Secret": process.env.BOT_USER_SECRET,
+                },
+            }
+        );
+
+        res.status(200).json({
+            text: response.data.choices[0].message.content,
+        });
     } catch (error) {
-        console.log(error);
+        console.error("error", error.response.data);
         res.status(500).json({ error: error.message });
     }
 });
